02bbb06196bd0f3e2e8286986d9aa59d9ba304f7
Encounter005 <1624221574@qq.com>
2023-10-31 20:51:55 +0800

    加了注释

--- C++/MyDataStructure/Stack.hpp
+++ C++/MyDataStructure/Stack.hpp
@@ -1,18 +1,22 @@
+#pragma once
 #include "Stack.h"
 
 template<typename T>
+// 初始化内存分配器
 std::allocator<T> Stack<T>::alloc;
 
 template<typename T>
+// @brief 重新分配内存
 void Stack<T>::reallocate() {
     T* newdata = nullptr;
-
+    // 如果发现容器是第一次使用，就直接初始化即可
     if(elements == nullptr || cap == nullptr) {
         newdata = alloc.allocate(1);
         elements = newdata;
         first_of_free = newdata;
         cap = newdata + 1;
     } else {
+        // 如果容器是内存已满要重新分配的话，那就开辟新的比原来大两倍的内存空间，再将数据拷贝过去
         newdata = alloc.allocate(size() * 2);
         auto dest = newdata;
         auto src = elements;
@@ -27,6 +31,7 @@ void Stack<T>::reallocate() {
 }
 
 template<typename T>
+//@brief 内存释放
 void Stack<T>::free() {
     if(elements != nullptr) {
         auto dest = elements;
@@ -39,6 +44,7 @@ void Stack<T>::free() {
 }
 
 template<typename T>
+//@brief 内存拷贝
 std::pair<T* , T*> Stack<T>::alloc_n_copy(const T* be , const T* ed) {
     auto newdata = alloc.allocate(ed - be);
     auto first_free = std::uninitialized_copy(be , ed , newdata);
@@ -46,6 +52,7 @@ std::pair<T* , T*> Stack<T>::alloc_n_copy(const T* be , const T* ed) {
 }
 
 template<typename T>
+//@brief 复制构造函数
 Stack<T>::Stack(const Stack& other) {
     auto copy = alloc_n_copy(other.begin() , other.end());
     elements = copy.first;
@@ -54,7 +61,8 @@ Stack<T>::Stack(const Stack& other) {
 }
 
 template<typename T>
-Stack<T>::Stack(Stack&& other) {
+//@brief 移动构造函数
+Stack<T>::Stack(Stack&& other) noexcept {
     auto copy = alloc_n_copy(other.begin() , other.end());
     elements = copy.first;
     first_of_free = copy.second;
@@ -62,6 +70,7 @@ Stack<T>::Stack(Stack&& other) {
 }
 
 template<typename T>
+//@brief 重载拷贝复制运算符
 Stack<T>& Stack<T>::operator=(const Stack& other) {
     if(this == &other) {
         return *this;
@@ -74,6 +83,7 @@ Stack<T>& Stack<T>::operator=(const Stack& other) {
 }
 
 template<typename T>
+//@brief 重载移动运算符
 Stack<T>& Stack<T>::operator=(Stack&& other) noexcept {
     if(this == &other) {
         return *this;
@@ -86,17 +96,21 @@ Stack<T>& Stack<T>::operator=(Stack&& other) noexcept {
 }
 
 template<typename T>
+// @brief 析构函数
 Stack<T>::~Stack() {
     free();
 }
 
 template<typename T>
+// @brief 插入函数
+// @param item 要添加的元素
 void Stack<T>::push(T item) {
     check_n_alloc();
     alloc.construct(first_of_free++ , item);
 }
 
 template<typename T>
+// @brief 删除函数，弹出栈顶
 void Stack<T>::pop() {
     if(first_of_free == nullptr) {
         return;
