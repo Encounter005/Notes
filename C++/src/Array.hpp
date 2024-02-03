25851144421a06c2c0a9c85e6f0376e476da9a6e
Encounter005 <1624221574@qq.com>
2024-02-03 21:31:22 +0800

    重写了Array

--- C++/src/Array.hpp
+++ C++/src/Array.hpp
@@ -1,213 +1,138 @@
 #pragma once
+
 #include <iostream>
-#include <cassert>
 #include <memory>
+#include <array>
+#include <algorithm>
+#include <stdexcept>
 #include <initializer_list>
-#include <type_traits>
-#include <cstddef>
 
-template <typename T> class TD;
-
-template <typename T> struct get_type {
-    using type = T;
-};
-
-template <typename T> struct get_type<T *> {
-    using type = T;
-};
-
-template <typename T> class Array {
+template <typename T, size_t SIZE = 0> class Array {
 public:
-    Array( size_t size_ );
-    Array( const std::initializer_list<T> &param );
-    Array( std::initializer_list<T> &&param );
-    Array( const Array &other );
-    Array( Array &&other );
-    ~Array();
-    void Insert( size_t pos, T value );
-    void Delete( size_t pos );
-    T find( size_t pos );
-    size_t cnt() const;
-    T &begin() const;
-    T &end() const;
-    T &operator[]( size_t pos ) const;
-    void clear();
-    bool empty();
+    Array() {
+        num_items = SIZE;
+        if ( num_items ) {
+            data_ = std::unique_ptr<T[]>( new T[num_items] );
+        } else {
+            data_ = nullptr;
+        }
+    }
+
+    Array( const std::initializer_list<T> &ilist ) {
+        num_items = ilist.size();
+        if ( ilist.size() ) {
+            size_t idx = 0;
+            data_      = std::unique_ptr<T[]>( new T[num_items]() );
+            for ( auto &elm : ilist ) {
+                data_[idx++] = elm;
+            }
+        } else {
+            data_ = nullptr;
+        }
+    }
+
+    Array( const Array &other ) {
+        num_items = other.num_items;
+        data_     = std::unique_ptr<T[]>( new T[num_items]() );
+        std::copy( other.begin(), other.end(), this->data_.get() );
+    }
+
+    Array &operator=( const Array &other ) {
+        if ( this == &other ) {
+            return *this;
+        }
+
+        if ( !this->empty() ) {
+            clear();
+        }
+
+        num_items = other.num_items;
+        data_     = std::unique_ptr( new T[num_items]() );
+        std::copy( other.begin(), other.end(), this->data_.get() );
+    }
+
+    ~Array() { clear(); }
+
+    template <typename U> void Insert( U &&element ) {
+        std::unique_ptr<T[]> new_data_( new T[num_items + 1]() );
+        for ( size_t i = 0; i < num_items; i++ ) {
+            new_data_[i] = data_[i];
+        }
+        new_data_[num_items] = element;
+        num_items++;
+        data_ = std::move( new_data_ );
+    }
+
+    template <typename U> void Insert( U &&element, size_t pos ) {
+        try {
+            if ( pos >= num_items ) {
+                throw std::runtime_error( "The index is larger than the size" );
+            }
+
+            std::unique_ptr<T[]> new_data_( new T[num_items + 1]() );
+            for ( size_t i = 0, j = 0; i < num_items; i++, j++ ) {
+                if ( i == pos ) {
+                    new_data_[j++] = element;
+                    new_data_[j]   = data_[i];
+                } else {
+                    new_data_[j] = data_[i];
+                }
+            }
+
+            data_ = std::move( new_data_ );
+            num_items++;
+        } catch ( const std::exception &e ) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+
+    void Delete( size_t pos ) {
+        if ( empty() ) {
+            return;
+        }
+        std::unique_ptr<T[]> new_data_( new T[num_items - 1]() );
+        for ( size_t i = 0, j = 0; i < num_items; i++, j++ ) {
+            if ( i == pos ) {
+                j--;
+                continue;
+            }
+            new_data_[j] = data_[i];
+        }
+
+        num_items--;
+        data_ = std::move( new_data_ );
+    }
+
+    T &operator[]( size_t idx ) {
+        try {
+            if ( idx >= num_items ) {
+                throw std::runtime_error("The index is larger than size");
+            }
+
+            return data_[idx];
+
+        } catch ( const std::exception &e ) {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+
+    size_t size() const { return num_items; }
+
+    bool empty() const { return num_items == 0; }
+
+    void clear() { num_items = 0; }
+
+    using iterator       = T *;
+    using const_iterator = const T *;
+
+    iterator begin() { return data_.get(); }
+    iterator end() { return data_.get() + num_items; }
+
+    const_iterator begin() const { return data_.get(); }
+
+    const_iterator end() const { return data_.get() + num_items; }
 
 private:
-    size_t size;
-    std::unique_ptr<T[]> data;
+    std::unique_ptr<T[]> data_;
+    size_t num_items;
 };
-
-template <typename T> Array<T>::Array( size_t size_ ) : size( size_ ) {
-    if ( size ) {
-        data = std::unique_ptr<T[]>( new T[size]() );
-    } else {
-        data = nullptr;
-    }
-}
-
-template <typename T> Array<T>::~Array() {
-    if ( data != nullptr ) {
-        data.release();
-    }
-    size = 0;
-}
-
-template <typename T> Array<T>::Array( const Array &other ) {
-    if ( data != nullptr ) {
-        data.release();
-    }
-    data( other.data );
-    other.data.release();
-}
-
-template <typename T> Array<T>::Array( Array &&other ) {
-
-    if ( data != nullptr ) {
-        data.release();
-    }
-    data = other.data;
-}
-
-/*
- *  Insert 插入函数
- *
- * @param pos 插入位置 value 数值
- * */
-
-template <typename T> void Array<T>::Insert( size_t pos, T value ) {
-    if ( pos > size ) {
-        assert( pos > size );
-    }
-    size++;
-    std::unique_ptr<T[]> other( new T[size]() );
-    for ( int i = 0, j = 0; i < size; i++ ) {
-        if ( i == pos - 1 ) {
-            other[i] = value;
-        } else {
-            other[i] = data[j++];
-        }
-    }
-    std::swap( data, other );
-    other.release();
-    other = nullptr;
-}
-
-/*
- * Delete 删除函数
- *
- * @param pos 删除位置
- **/
-
-template <typename T> void Array<T>::Delete( size_t pos ) {
-    if ( pos > size ) {
-        assert( pos > size );
-    }
-    std::unique_ptr<T[]> other( new T[size - 1]() );
-    for ( int i = 0, j = 0; i < size; i++ ) {
-        if ( i != pos - 1 ) {
-            other[j++] = data[i];
-        }
-    }
-    std::swap( other, data );
-    other.release();
-    other = nullptr;
-    size--;
-}
-
-/*
- * Find 查找函数
- *
- * @param pos 查找位置
- *
- * @return 这个位置上的元素
- *
- * */
-
-template <typename T> T Array<T>::find( size_t pos ) {
-    if ( pos < 0 || pos > size ) {
-        return -1;
-    }
-    return data[pos - 1];
-}
-
-/*
- * begin
- *
- * @return 返回数组的头
- *
- * */
-
-template <typename T> T &Array<T>::begin() const { return data[0]; }
-
-/*
- * end
- *
- * @return 返回数组的末尾
- *
- * */
-
-template <typename T> T &Array<T>::end() const { return data[size - 1]; }
-
-/*
- * cnt
- *
- * @return 数组的大小
- * */
-
-template <typename T> size_t Array<T>::cnt() const { return size; }
-
-template <typename T> Array<T>::Array( std::initializer_list<T> &&param ) {
-    if ( data != nullptr ) {
-        data.release();
-        size = 0;
-        data = nullptr;
-    }
-
-    if ( param.size() ) {
-        unsigned int cnt = 0;
-        data             = std::unique_ptr<T[]>( new T[param.size()] );
-        for ( const auto &elem : param ) {
-            data[cnt++] = elem;
-        }
-        size = cnt;
-    } else {
-        data = nullptr;
-    }
-}
-
-template <typename T> Array<T>::Array( const std::initializer_list<T> &param ) {
-    if ( data != nullptr ) {
-        data.release();
-        size = 0;
-        data = nullptr;
-    }
-
-    if ( param.size() ) {
-        unsigned int cnt = 0;
-        data             = std::unique_ptr<T[]>( new T[param.size()] );
-        for ( const auto &elem : param ) {
-            data[cnt++] = elem;
-        }
-        size = cnt;
-    } else {
-        data = nullptr;
-    }
-}
-
-template <typename T> T &Array<T>::operator[]( size_t pos ) const {
-    if ( pos >= size ) {
-        assert( pos >= size );
-    } else {
-        return data[pos];
-    }
-}
-
-template <typename T> void Array<T>::clear() {
-    data.release();
-    data = nullptr;
-    size = 0;
-}
-
-template <typename T> bool Array<T>::empty() { return size == 0; }
